189cb26f71dd8cec0a30a89f59fb44edc1ef6a1d
CiroKite <cirotosta@gmail.com>
2019-08-26 13:04:50 -0300

    Added nanoid size as param

--- run.js
+++ run.js
@@ -2,7 +2,8 @@
 
 const nanoid = require('nanoid');
 const count = process.argv[2] | 3;
+const size  = process.argv[3] | 15;
 
 for (let i = 0; i < count; i++){
-	console.log(nanoid(15));
+	console.log(nanoid(size));
 }
