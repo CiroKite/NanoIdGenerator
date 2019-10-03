43f6dcf6732bac509158aec45db3a7120c280ad4
CiroKite <cirotosta@gmail.com>
2019-10-03 17:24:45 -0300

    Fixed or operator

--- run.js
+++ run.js
@@ -1,8 +1,8 @@
 'use strict';
 
 const nanoid = require('nanoid');
-const count = process.argv[2] | 3;
-const size  = process.argv[3] | 15;
+const count = process.argv[2] || 3;
+const size  = process.argv[3] || 15;
 
 for (let i = 0; i < count; i++){
 	console.log(nanoid(size));
